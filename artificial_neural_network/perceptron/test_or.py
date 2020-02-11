804b5a13b8a511204d47639d922cbab34b25ddca
Mohammad Hossein <iammhz77@gmail.com>
2020-02-11 21:43:45 +0330

    changed alpha and epochs

--- artificial_neural_network/perceptron/test_or.py
+++ artificial_neural_network/perceptron/test_or.py
@@ -4,8 +4,8 @@ from artificial_neural_network.perceptron.perceptron import Perceptron
 x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 y = np.array([[0], [1], [1], [1]])
 
-perceptron = Perceptron(x.shape[1], alpha=0.1)
-perceptron.fit(x, y, epochs=22)
+perceptron = Perceptron(x.shape[1])
+perceptron.fit(x, y)
 
 # now that our network is trained, loop over the data points
 for (data, target) in zip(x, y):

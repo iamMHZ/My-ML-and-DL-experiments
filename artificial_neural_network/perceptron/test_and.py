f335fc8d9a789e2321fc749345e3ad1900af9d38
Mohammad Hossein <iammhz77@gmail.com>
2020-02-11 21:49:19 +0330

    predict data with AND dataset

--- artificial_neural_network/perceptron/test_and.py
+++ artificial_neural_network/perceptron/test_and.py
@@ -1,9 +1,12 @@
 from artificial_neural_network.perceptron.perceptron import Perceptron
+import numpy as np
 
-x = [[0, 0], [0, 1], [1, 0], [1, 1]]
-y = [[0], [0], [0], [1]]
+x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([[0], [0], [0], [1]])
 
 perceptron = Perceptron(2)
 
 perceptron.fit(x, y)
-perceptron.predict(x)
+for (data, target) in zip(x, y):
+    prediction = perceptron.predict(data)
+    print(f"[TESTING] data={data}, ground-truth={target}, prediction={prediction}")

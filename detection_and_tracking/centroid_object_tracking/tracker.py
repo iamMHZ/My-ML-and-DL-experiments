ca6a87608e93613d282f08c81583336e6e308e35
Mohammad Hossein <iammhz77@gmail.com>
2020-02-25 19:46:23 +0330

    bug : scale of centroids

--- detection_and_tracking/centroid_object_tracking/tracker.py
+++ detection_and_tracking/centroid_object_tracking/tracker.py
@@ -14,6 +14,10 @@ class Distance:
     def __lt__(self, other):
         return self.euclidean_distance > other.euclidean_distance
 
+    def __repr__(self):
+        return 'from ' + str(self.start_index) + ' to  ' + str(self.end_index) + ' with distance ' + str(
+            self.euclidean_distance)
+
 
 class CentroidTracker:
 
@@ -49,7 +53,8 @@ class CentroidTracker:
 
         else:
             # computer centroid from bounding boxes
-            input_centroids = np.zero(len(bounding_boxes), 2)
+            input_centroids = np.zeros((len(bounding_boxes), 2), dtype="int")
+            input_centroids = list(input_centroids)
 
             for (i, (start_x, start_y, end_x, end_y)) in enumerate(bounding_boxes):
                 # compute centroid for each bounding box
@@ -65,30 +70,33 @@ class CentroidTracker:
             # computer distances and update objects
             else:
 
-                existing_object_ids = list(self.objects.keys())
-                existing_centroids = list(self.objects.values())
+                existing_object_ids = list(self.objects.keys())[:]
+                existing_centroids = list(self.objects.values())[:]
 
                 number_of_input_centroid = len(input_centroids)
                 number_of_existing_centroids = len(existing_object_ids)
 
                 # compute distance between new centroids and old existing ones:
                 # TODO : improve time complexity of this part :
-                for i in enumerate(input_centroids):
+                for i, c1 in enumerate(input_centroids):
                     distances = []
-                    for j in enumerate(existing_centroids):
-                        euclidean_distance = self.compute_euclidean_distance(input_centroids[i], existing_centroids[j])
+                    for j, c2 in enumerate(existing_centroids):
+                        euclidean_distance = self.compute_euclidean_distance(c1, c2)
 
                         distance = Distance(i, j, euclidean_distance)
                         distances.append(distance)
 
                     # now sort distances from i-th input to existing centroid
                     distances = sorted(distances)
+                    print(distances)
                     candidate = distances[0]
 
                     index = candidate.end_index
+                    key = existing_object_ids[index]
                     # update existing object centroid
-                    self.objects[index] = input_centroids[i]
-                    del input_centroids[i]
+                    self.objects[key] = input_centroids[i]
+                    # del input_centroids[i]
+                    input_centroids[i] = None
                     del existing_centroids[index]
                     del existing_object_ids[index]
 
@@ -96,14 +104,16 @@ class CentroidTracker:
                 # TODO : remove code duplication
                 if number_of_existing_centroids >= number_of_input_centroid:
                     for id in existing_object_ids:
-                        self.disappeared[id] += 1
-                        # check if current object has to be considered lost
-                        if self.disappeared[id] > self.max_disappeared:
-                            self.unregister_object(id)
+                        if id is not None:
+                            self.disappeared[id] += 1
+                            # check if current object has to be considered lost
+                            if self.disappeared[id] > self.max_disappeared:
+                                self.unregister_object(id)
                 else:
                     # register new objects
                     for centroid in input_centroids:
-                        self.register_object(centroid)
+                        if centroid is not None:
+                            self.register_object(centroid)
 
         return self.objects
 

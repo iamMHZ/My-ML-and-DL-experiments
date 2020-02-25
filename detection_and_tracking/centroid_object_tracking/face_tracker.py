06e5e5ac8bb851c9ea3ba032af482bd650a9df2f
Mohammad Hossein <iammhz77@gmail.com>
2020-02-25 19:45:54 +0330

    testing tracker with tracing faces

---
+++ detection_and_tracking/centroid_object_tracking/face_tracker.py
@@ -0,0 +1,44 @@
+import cv2
+
+from detection_and_tracking.centroid_object_tracking.tracker import CentroidTracker
+
+
+def show_frame(frame, window_name, delay=1):
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+    cv2.imshow(window_name, frame)
+    cv2.waitKey(delay)
+
+
+def main():
+    camera = cv2.VideoCapture(0)
+    face_cascade_path = './haarcascade_frontalface_default.xml'
+
+    classifier = cv2.CascadeClassifier(face_cascade_path)
+
+    centroid_tracker = CentroidTracker()
+
+    while camera.isOpened():
+
+        rectangles = []
+        _, frame = camera.read()
+
+        faces = classifier.detectMultiScale(frame)
+
+        for face in faces:
+            # print(faces)
+            x, y, w, h = face
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (80, 120, 30))
+
+            rectangles.append(face)
+
+        objects = centroid_tracker.update(rectangles)
+
+        for id, centroid in objects.items():
+            cv2.circle(frame, center=centroid, radius=5, color=(0, 0, 200), thickness=-1)
+            print(id, centroid, sep=' in ')
+
+        show_frame(frame, 'frame')
+
+
+if __name__ == '__main__':
+    main()

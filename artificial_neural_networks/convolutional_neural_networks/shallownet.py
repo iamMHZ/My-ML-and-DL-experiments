34d839f8684b6b5680169282fe61c2326c6c14b2
Mohammad Hossein <iammhz77@gmail.com>
2020-02-26 09:41:05 +0330

    venv

---
+++ artificial_neural_networks/convolutional_neural_networks/shallownet.py
@@ -0,0 +1,15 @@
+from keras.models import Sequential
+from keras.layers.core import Dense
+from keras.layers.core import activations
+from keras.layers.convolutional import Conv2D
+from keras.layers.core import Flatten
+from keras import backend as K
+
+
+
+class ShallowNet:
+
+    @staticmethod
+    def build(width ,height ,channels):
+        pass
+

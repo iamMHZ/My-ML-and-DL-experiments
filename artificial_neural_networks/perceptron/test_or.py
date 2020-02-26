a5860ce6809676043e047d626e4b85aaa0fceea7
Mohammad Hossein <iammhz77@gmail.com>
2020-02-26 09:09:48 +0330

    refactoring

--- artificial_neural_networks/perceptron/test_or.py
+++ artificial_neural_networks/perceptron/test_or.py
@@ -1,12 +1,14 @@
-from artificial_neural_network.perceptron.perceptron import Perceptron
 import numpy as np
+from artificial_neural_networks.perceptron.perceptron import Perceptron
 
 x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-y = np.array([[0], [0], [0], [1]])
+y = np.array([[0], [1], [1], [1]])
 
-perceptron = Perceptron(2)
-
+perceptron = Perceptron(x.shape[1])
 perceptron.fit(x, y)
+
+# now that our network is trained, loop over the data points
 for (data, target) in zip(x, y):
     prediction = perceptron.predict(data)
+
     print(f"[TESTING] data={data}, ground-truth={target}, prediction={prediction}")
--- artificial_neural_networks/perceptron/test_xor.py
+++ artificial_neural_networks/perceptron/test_xor.py
@@ -1,14 +1,12 @@
+from artificial_neural_networks.perceptron.perceptron import Perceptron
 import numpy as np
-from artificial_neural_network.perceptron.perceptron import Perceptron
 
 x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-y = np.array([[0], [1], [1], [1]])
+y = np.array([[0], [1], [1], [0]])
 
-perceptron = Perceptron(x.shape[1])
-perceptron.fit(x, y)
+perceptron = Perceptron(2,alpha=0.001)
 
-# now that our network is trained, loop over the data points
+perceptron.fit(x, y,epochs=2000)
 for (data, target) in zip(x, y):
     prediction = perceptron.predict(data)
-
     print(f"[TESTING] data={data}, ground-truth={target}, prediction={prediction}")

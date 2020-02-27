7580e35f55463528183d1fcea67916672093a738
Mohammad Hossein <iammhz77@gmail.com>
2020-02-27 09:42:06 +0330

    BUG in encoding labels

--- artificial_neural_networks/convolutional_neural_networks/shallownet_animals.py
+++ artificial_neural_networks/convolutional_neural_networks/shallownet_animals.py
@@ -1,10 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.optimizers import SGD
+from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelBinarizer
 
+from artificial_neural_networks.convolutional_neural_networks.shallownet import ShallowNet
 from loaders.image_loader import ImageLoader
 from preprocessors.image_preprocessor import ImageToArrayPreprocessor, ResizePreprocessor
 
-from sha
 image_to_array = ImageToArrayPreprocessor()
 resizer = ResizePreprocessor(32, 32)
 
@@ -12,11 +16,13 @@ loader = ImageLoader(preprocessors=[resizer, image_to_array])
 
 # loading animal dataset
 print('\n[INFO] Loading data...\n')
+
 data, labels = loader.load(path='../../datasets/cats_and_dogs')
 
 data = data.astype('float') / 255.0
 
 print('[INFO] splitting dataset...')
+
 # splitting dataset:
 train_x, test_x, train_y, test_y = train_test_split(data, labels, test_size=0.25, random_state=42)
 
@@ -26,5 +32,33 @@ lb = LabelBinarizer()
 test_y = lb.fit_transform(test_y)
 train_y = lb.fit_transform(train_y)
 
+# building model
+model = ShallowNet.build(32, 32, 3, 2)
+
 print('[INFO] COMPILING MODEL...')
-model =
+sgd = SGD(learning_rate=0.005)
+model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+
+# training model
+print('[INFO] TRAINING ...')
+model_history = model.fit(train_x, train_y, batch_size=32, epochs=100, validation_data=(test_x, test_y), verbose=1)
+
+# evaluating network
+print('[INFO] EVALUATING network...')
+predictions = model.predict(test_x, batch_size=32, verbose=1)
+print(classification_report(test_y.argmax(axis=1), predictions.argmax(axis=1), target_names=["cat", "dog"]))
+
+# plot the training loss and accuracy
+print('[INFO] plotting...')
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, 100), model_history.history["loss"], label="train_loss")
+plt.plot(np.arange(0, 100), model_history.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, 100), model_history.history["accuracy"], label="train_acc")
+plt.plot(np.arange(0, 100), model_history.history["val_accuracy"], label="val_acc")
+plt.title("Training Loss and Accuracy")
+plt.xlabel("Epoch")
+plt.ylabel("Loss/Accuracy")
+plt.legend()
+plt.savefig('ShallowNetAnimals.png')
+plt.show()

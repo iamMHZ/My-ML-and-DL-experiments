98db509293054ba36ee221925ea20a65eabcf509
Mohammad Hossein <iammhz77@gmail.com>
2020-03-04 17:07:47 +0330

    testing shallowNet on two group of signs

---
+++ detection_and_tracking/sign_detection/shallownet_test.py
@@ -0,0 +1,75 @@
+from keras.optimizers import SGD
+from keras.utils import np_utils
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+
+from artificial_neural_networks.convolutional_neural_networks.shallownet.shallownet import ShallowNet
+from loaders.image_loader import ImageLoader
+from monitoring.training_monitoring import TrainingMonitor
+from preprocessors.image_preprocessor import ImageToArrayPreprocessor, ResizePreprocessor
+
+input_width = 32
+input_height = 32
+input_depth = 3
+
+num_classes = 2
+
+batch_size = 32
+epochs = 100
+learning_rate = 0.0001
+
+
+def train_and_test(data, labels):
+    # splitting dataset
+    # data = data.astype('float') / 255.0
+    x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.25)
+
+    # convert labels to vectors
+    lb = LabelBinarizer()
+    y_test = lb.fit_transform(y_test)
+    y_train = lb.fit_transform(y_train)
+
+    y_test = np_utils.to_categorical(y_test, num_classes=num_classes)
+    y_train = np_utils.to_categorical(y_train, num_classes=num_classes)
+
+    print('[INFO] DATA PREPARED')
+
+    # build model
+    model = ShallowNet.build(input_width, input_height, input_depth, num_classes)
+
+    print('[INFO] COMPILING MODEL...')
+    sgd = SGD(learning_rate=learning_rate)
+    model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+
+    print('[INFO] TRAINING...')
+
+    callbacks = [TrainingMonitor('shallowNetHazmat')]
+    model_history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1,
+                              validation_data=(x_test, y_test),
+                              callbacks=callbacks)
+
+    # evaluating network
+    print('[INFO] EVALUATING network...')
+    predictions = model.predict(x_test, batch_size=batch_size, verbose=1)
+    print(classification_report(y_test.argmax(axis=1), predictions.argmax(axis=1),
+                                target_names=[str(l) for l in lb.classes_]))
+
+
+def main():
+    dataset_path = 'D:\Programming\database of image\HAZMAT\Datasets\smallDataset'
+
+    image_to_array = ImageToArrayPreprocessor()
+    resizer = ResizePreprocessor(input_width, input_height)
+
+    loader = ImageLoader(preprocessors=[resizer, image_to_array])
+
+    data, labels = loader.load(dataset_path)
+
+    # pprint.pprint(labels)
+
+    train_and_test(data, labels)
+
+
+if __name__ == '__main__':
+    main()

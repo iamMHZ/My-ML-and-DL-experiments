410a81e5d2b5337efc844ac776a79e8f93fc1ab9
Mohammad Hossein <iammhz77@gmail.com>
2020-04-15 16:45:10 +0430

    AlexNet

--- artificial_neural_networks/convolutional_neural_networks/alex_net/alex_net_model.py
+++ artificial_neural_networks/convolutional_neural_networks/alex_net/alex_net_model.py
@@ -21,12 +21,10 @@ class AlexNet:
 
         model.add(Conv2D(filters=96, input_shape=input_shape, kernel_size=(11, 11), strides=(4, 4), padding='valid'))
         model.add(Activation('relu'))
-        # Max Pooling
         model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid'))
 
         model.add(Conv2D(filters=256, kernel_size=(11, 11), strides=(1, 1), padding='valid'))
         model.add(Activation('relu'))
-        # Max Pooling
         model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid'))
 
         model.add(Conv2D(filters=384, kernel_size=(3, 3), strides=(1, 1), padding='valid'))

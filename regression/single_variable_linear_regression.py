dff8bde40990a0e1f5db81b586dafb8321e8cbe6
Mohammad Hossein <iammhz77@gmail.com>
2020-05-02 19:35:24 +0430

    some of squared loss function

--- regression/single_variable_linear_regression.py
+++ regression/single_variable_linear_regression.py
@@ -14,20 +14,30 @@ def plot(x, y, w):
 
     line = np.matmul(x, w)
 
+    # x[:, 1:] we discard bios column
     plt.scatter(x[:, 1:], y)
-    plt.plot(x[:, 1:], line)
+    plt.plot(x[:, 1:], line, c='r')
     plt.show()
 
 
-def loss(x, y, w):
+def compute_loss(x, y, w):
     """
-     Plots the data and the pridicted line
+     calculates the sum of squared loss
 
     :param x: the data
     :param y:  grand truth labels
     :param w:  the predicted line
-    :return: None
+    :return: sum of squared loss
     """
+
+    predictions = np.matmul(x, w)
+
+    errors = predictions - y
+
+    loss = 0.5 * np.sum(errors ** 2)
+
+    print(f'Errors {errors}')
+    print(f'sum of squared loss {loss}')
 
 
 if __name__ == '__main__':
@@ -37,3 +47,4 @@ if __name__ == '__main__':
     w = np.array([[0], [1]])
 
     plot(x, y, w)
+    compute_loss(x, y, w)

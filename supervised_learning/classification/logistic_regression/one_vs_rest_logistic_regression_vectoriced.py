d516706cbf8794132e3355c1c63773f000df21af
Mohammad Hossein <iammhz77@gmail.com>
2020-10-20 18:41:27 +0330

    one vs rest logistic regression skeleton

--- supervised_learning/classification/logistic_regression/one_vs_rest_logistic_regression_vectoriced.py
+++ supervised_learning/classification/logistic_regression/one_vs_rest_logistic_regression_vectoriced.py
@@ -1,3 +1,21 @@
 """
 Vectorized implementation of the one versus rest logistic regression classifier
 """
+
+
+class LogisticRegressionOVR:
+
+    def __init__(self):
+        pass
+
+    def fit(self, train_x, train_y, epochs, learning_rate):
+        pass
+
+    def get_loss(self, y_pred, y_true):
+        pass
+
+    def get_gradients(self, train_x, y_pred, y_true):
+        pass
+
+    def sigmoid(self, x):
+        pass

6cdea934be71f81558f655ffc1f612e0d21b4e71
Mohammad Hossein <iammhz77@gmail.com>
2020-10-25 19:22:16 +0330

    softmax classifier skeleton

---
+++ supervised_learning/classification/softmax_classifer/softmax_classifier.py
@@ -0,0 +1,22 @@
+
+class SoftmaxClassifer:
+
+    def __init__(self):
+        pass
+    def fit(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def softmax(self):
+        pass
+
+    def loss(self):
+        pass
+
+    def get_gradients(self):
+        pass
+
+    def plot(self):
+        pass

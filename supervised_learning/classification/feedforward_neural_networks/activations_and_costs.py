43e0dd6dd0b420bd8eb1a9be135d1ca4d983404a
Mohammad Hossein <iammhz77@gmail.com>
2020-12-02 21:32:38 +0330

    activation is abstract for a while 😁

--- supervised_learning/classification/feedforward_neural_networks/activations_and_costs.py
+++ supervised_learning/classification/feedforward_neural_networks/activations_and_costs.py
@@ -1,5 +1,16 @@
-class Activation:
+from abc import ABC, abstractmethod
+
+
+class Activation(ABC):
     def __init__(self):
+        pass
+
+    @abstractmethod
+    def make_activation(self, x):
+        pass
+
+    @abstractmethod
+    def activation_derivatives(self):
         pass
 
 

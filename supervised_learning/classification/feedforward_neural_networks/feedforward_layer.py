99c3f30ccb9f785aa05b91203016d434c2e987f5
Mohammad Hossein <iammhz77@gmail.com>
2020-12-02 21:26:07 +0330

    constructor of the layer class

--- supervised_learning/classification/feedforward_neural_networks/feedforward_layer.py
+++ supervised_learning/classification/feedforward_neural_networks/feedforward_layer.py
@@ -1,3 +1,20 @@
+import numpy as np
+
+
 class FeedForwardLayer:
-    def __init__(self):
+    def __init__(self, num_neurons, activation_function):
+        self.num_neurons = num_neurons
+        self.activation_function = activation_function
+
+        # INITIALIZE
+        self.weighted_input = np.zeros(shape=(self.num_neurons, 1))
+        self.layer_activations = np.zeros(shape=(self.num_neurons, 1))
+        # TODO how to determine the shape of weights dynamically
+        self.weights = None
+        self.biases = np.zeros(shape=(self.num_neurons, 1))
+
+    def forward(self):
+        pass
+
+    def backward(self):
         pass

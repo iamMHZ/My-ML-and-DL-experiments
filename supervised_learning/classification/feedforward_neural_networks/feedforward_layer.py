999e0198fc2da178b034ec09030405d38c2d1544
Mohammad Hossein <iammhz77@gmail.com>
2020-12-05 20:20:21 +0330

    changed the initialization of weight and biases

--- supervised_learning/classification/feedforward_neural_networks/feedforward_layer.py
+++ supervised_learning/classification/feedforward_neural_networks/feedforward_layer.py
@@ -9,19 +9,19 @@ class FeedForwardLayer:
         self.activation_function = activation_function
 
         # INITIALIZE
-        self.layer_weighted_input = np.zeros(shape=(self.num_neurons, 1))
-        self.layer_activations = np.zeros(shape=(self.num_neurons, 1))
+        self.layer_weighted_input = np.zeros(shape=(self.num_neurons, 1))  # OR None ?
+        self.layer_activations = np.zeros(shape=(self.num_neurons, 1))  # OR None ?
         # TODO how to determine the shape of weights dynamically
         # TODO  the if in self.forward is doing it but is there a better way
         self.layer_weights = None
-        self.layer_biases = np.zeros(shape=(self.num_neurons, 1))
+        self.layer_biases = np.random.rand(self.num_neurons, 1)
 
     def forward(self, previous_activation):
         # initialize weights
         if self.layer_weights is None:
             # TODO is there any other way of doing the initialization here
             # TODO like using model class or sth else
-            self.layer_weights = np.zeros(shape=(previous_activation.shape[1], self.num_neurons))
+            self.layer_weights = np.random.rand(previous_activation.shape[1], self.num_neurons)
 
         self.layer_weighted_input = np.matmul(previous_activation, self.layer_weights) + self.layer_biases
         self.layer_activations = self.activation_function.make_activation(self.layer_weighted_input)

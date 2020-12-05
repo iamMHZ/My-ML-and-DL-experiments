862af772f6a5e94d7291e05456d6b01630a187c9
Mohammad Hossein <iammhz77@gmail.com>
2020-12-05 19:14:08 +0330

    sigmoid activation function

--- supervised_learning/classification/feedforward_neural_networks/activations_and_costs.py
+++ supervised_learning/classification/feedforward_neural_networks/activations_and_costs.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+import numpy as np
+
 
 class ActivationFunction(ABC):
     def __init__(self):
@@ -10,7 +12,30 @@ class ActivationFunction(ABC):
         pass
 
     @abstractmethod
-    def activation_derivatives(self):
+    def activation_derivatives(self, x):
+        pass
+
+
+class SigmoidActivationFunction(ActivationFunction):
+    def __init__(self):
+        super().__init__()
+
+    def make_activation(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def activation_derivatives(self, x):
+        # The derivative of sigmoid is : sigmoid(x) * (1-sigmoid(x))
+        return self.make_activation(x) * (1 - self.make_activation(x))
+
+
+class SoftmaxActivationFunction(ActivationFunction):
+    def __init__(self):
+        super().__init__()
+
+    def make_activation(self, x):
+        pass
+
+    def activation_derivatives(self, x):
         pass
 
 

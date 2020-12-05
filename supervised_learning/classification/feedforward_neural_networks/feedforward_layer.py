bba1714176b5d42cb1dcb6254677873b2fdaec0c
Mohammad Hossein <iammhz77@gmail.com>
2020-12-05 19:22:36 +0330

    weight initialization and forward pass are finished

--- supervised_learning/classification/feedforward_neural_networks/feedforward_layer.py
+++ supervised_learning/classification/feedforward_neural_networks/feedforward_layer.py
@@ -12,12 +12,21 @@ class FeedForwardLayer:
         self.layer_weighted_input = np.zeros(shape=(self.num_neurons, 1))
         self.layer_activations = np.zeros(shape=(self.num_neurons, 1))
         # TODO how to determine the shape of weights dynamically
+        # TODO  the if in self.forward is doing it but is there a better way
         self.layer_weights = None
         self.layer_biases = np.zeros(shape=(self.num_neurons, 1))
 
-    def forward(self, data):
-        self.layer_weighted_input = np.matmul(data, self.layer_weights)
+    def forward(self, previouse_activation):
+        # initialize weights
+        if self.layer_weights is None:
+            # TODO is there any other way of doing the initialization here
+            # TODO like using model class or sth else
+            self.layer_weights = np.zeros(shape=(previouse_activation.shape[1], self.num_neurons))
+
+        self.layer_weighted_input = np.matmul(previouse_activation, self.layer_weights)
         self.layer_activations = self.activation_function.make_activation(self.layer_weighted_input)
+
+        return self.layer_activations
 
     def backward(self):
         pass

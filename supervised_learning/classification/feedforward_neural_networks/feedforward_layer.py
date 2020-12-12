backprop bug

supervised_learning/classification/feedforward_neural_networks/feedforward_layer.py
@@ -17,6 +17,7 @@ class FeedForwardLayer:
         self.layer_biases = np.random.rand(1, self.num_neurons)
 
         self.previous_activation = None
+        self.layer_error = None
 
     def forward(self, previous_activation):
         self.previous_activation = previous_activation
@@ -37,11 +38,17 @@ class FeedForwardLayer:
     def backward(self, up_coming_error, learning_rate):
         # TODO check and complete backward pass
         # TODO separate the optimization part
-        error = up_coming_error * self.activation_function.activation_derivatives(self.layer_weighted_input)
+        self.layer_error = up_coming_error * self.activation_function.activation_derivatives(self.layer_weighted_input)
 
         # Update the weights and biases
 
-        self.layer_weights -= learning_rate * np.sum(error * self.previous_activation.T, axis=0)
-        self.layer_biases -= learning_rate * np.sum(error, axis=1)
+        self.update_weights(learning_rate)
 
-        return error
+        # (W layer + 1.T @ delta layer + 1)
+        self.layer_error = self.layer_error @ self.layer_weights.T
+
+        return self.layer_error
+
+    def update_weights(self, learning_rate):
+        self.layer_biases += -learning_rate * np.sum(self.layer_error, axis=0)
+        self.layer_weights += -learning_rate * (self.previous_activation.T @ self.layer_error)

supervised_learning/classification/feedforward_neural_networks/main.py
@@ -19,6 +19,6 @@ data = np.random.randint(0, 10, size=(120, 3))
 labels = np.random.randint(0, 2, size=(120, 1))
 
 # TODO hot encode labels
-model.train(data, labels, epochs=2)
+model.train(data, labels, epochs=100, learning_rate=0.0001)
 
-print(model.evaluate(data[0]))
+# print(model.evaluate(data[0]))

supervised_learning/classification/feedforward_neural_networks/neural_network.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from supervised_learning.classification.feedforward_neural_networks.activations_and_costs import CostFunction
 
 
@@ -11,10 +13,12 @@ class FeedForwardNeuralNetwork:
 
         self.cost_function = cost_fuction
 
+        self.losses = []
+
     def add_layer(self, layer):
         self.layers.append(layer)
 
-    def train(self, train_x, train_y, epochs=1, learning_rate=0.01):
+    def train(self, train_x, train_y, epochs=10, learning_rate=0.01):
         # TODO check backward pass
 
         for j in range(epochs):
@@ -30,6 +34,8 @@ class FeedForwardNeuralNetwork:
 
             epoch_loss = self.cost_function.cost(train_y, activation)
             print('Epoch {}, Loss {} '.format(j, epoch_loss))
+            # for plotting purposes
+            self.losses.append(epoch_loss)
 
             # backward pass
 
@@ -37,9 +43,19 @@ class FeedForwardNeuralNetwork:
                               self.layers[-1].activation_function.activation_derivatives(
                                   self.layers[-1].layer_weighted_input)
 
-            up_coming_error =  up_coming_error @ self.layers[-1].layer_weights.T
+            # update last layer weights and biases
+            self.layers[-1].layer_error = up_coming_error
 
-            for i in range(-len(self.layers) + 1, 0):
+            self.layers[-1].layer_biases += -learning_rate * np.sum(self.layers[-1].layer_error, axis=0)
+            self.layers[-1].layer_weights += -learning_rate * (
+                    self.layers[-1].previous_activation.T @ self.layers[-1].layer_error)
+
+            # (W layer + 1 .T @ delta layer + 1 )
+            last_layer_weights = self.layers[-1].layer_weights.T
+
+            up_coming_error = up_coming_error @ last_layer_weights
+
+            for i in range(-2, -len(self.layers) - 1, -1):
                 up_coming_error = self.layers[i].backward(up_coming_error, learning_rate)
 
     def evaluate(self, data):
@@ -55,4 +71,7 @@ class FeedForwardNeuralNetwork:
         return activation
 
     def predict(self):
+        pass
+
+    def on_training_ended(self):
         pass